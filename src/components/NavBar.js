de28b92333e1c453647121be879b5bbdac60f3b8
gabiota2018 <gabisago6@gmail.com>
2021-06-16 23:54:55 -0300

    commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CartWidget  from './CartWidget';
+
+function NavBar() {
+  return (
+    <div style={{width:'100%',height:'100px', background:'rgb(97,218,215)'}}>
+       <CartWidget/>
+    </div>
+  )
+}
+
+export default NavBar;
